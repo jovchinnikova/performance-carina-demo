684ba4daa6ea6a6066f6530dd8dbad3ca6bd5804
Julia Ovchinnikova <jovchinnikova@solvd.com>
2023-05-22 14:32:29 +0300

    Fix for testLoginUser

--- src/test/java/com/performance/demo/carina/MobileSampleTest.java
+++ src/test/java/com/performance/demo/carina/MobileSampleTest.java
@@ -31,7 +31,7 @@ public class MobileSampleTest implements IAbstractTest, IMobileUtils, IPerforman
     @Test()
     @MethodOwner(owner = "jovchinnikova")
     @TestLabel(name = "feature", value = {"mobile", "regression"})
-    @PerformanceTest(flowName = "login_flow", userName = "Test_user", collectLoginTime = false, collectExecutionTime = true)
+    @PerformanceTest(flowName = "login_flow", userName = "Test_user", collectLoginTime = true, collectExecutionTime = false)
     public void testLoginUser() {
         String password = RandomStringUtils.randomAlphabetic(10);
         WelcomePageBase welcomePage = initPage(getDriver(), WelcomePageBase.class);
