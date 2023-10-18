4595227b6fdf08a2abb89232214710c852bb7bc6
Julia Ovchinnikova <jovchinnikova@solvd.com>
2023-10-18 18:51:36 +0300

    Update netParser to fit android 14

--- src/main/java/com/performance/demo/utils/parser/NetParser2.java
+++ src/main/java/com/performance/demo/utils/parser/NetParser2.java
@@ -1,39 +1,45 @@
 package com.performance.demo.utils.parser;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.lang.invoke.MethodHandles;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NetParser2 {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    private NetParser.NetRow row;
-
-    private static final Pattern VALUES = Pattern.compile("\\swlan0: *(\\d*) *(\\d*) * *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*)");
+    private static final Pattern VALUES = Pattern.compile(
+            "\\s(wlan[01]): *(\\d*) *(\\d*) * *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*) *(\\d*)");
 
     public NetParser.NetRow parse(List<String> lines) {
         int st = 0;
-        long rb;
-        long rp;
-        long tb;
-        long tp;
+        Map<String, NetParser.NetRow> netData = new HashMap<>();
+        String expectedType;
 
-        for (String line : lines) {
+        lines.forEach(line -> {
             Matcher matcher = VALUES.matcher(line);
             if (matcher.matches()) {
-                LOGGER.info("Matcher " + matcher);
-                rb = Long.parseLong(matcher.group(1));
-                rp = Long.parseLong(matcher.group(2));
-                tb = Long.parseLong(matcher.group(9));
-                tp = Long.parseLong(matcher.group(10));
-                row = new NetParser.NetRow(st, rb, rp, tp, tb);
+                String type = matcher.group(1);
+                long rb = Long.parseLong(matcher.group(2));
+                long rp = Long.parseLong(matcher.group(3));
+                long tb = Long.parseLong(matcher.group(10));
+                long tp = Long.parseLong(matcher.group(11));
+                if (rb != 0 && rp != 0 && tb != 0 && tp != 0)
+                    netData.put(type, new NetParser.NetRow(st, rb, rp, tp, tb));
             }
-        }
-        return row;
+        });
+
+        if (netData.size() == 2)
+            expectedType = "wlan1";
+        else expectedType = "wlan0";
+
+        LOGGER.info("Net data type: {}", expectedType);
+        return netData.get(expectedType);
     }
 }
